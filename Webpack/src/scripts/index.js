881f77010d613f048fd7a8b55a78d24dfd8e8e9a
Tor020 <kopp020@gmail.com>
2023-06-18 02:38:04 -0400

    default ahk commit message

---
+++ Webpack/src/scripts/index.js
@@ -0,0 +1,10 @@
+import '../styles/index.scss';
+import '@fortawesome/fontawesome-free';
+
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+// import  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css'
+
+console.log('webpack starterkit');
